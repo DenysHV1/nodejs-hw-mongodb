9377d3a390d98dc09b7875e67c145f02814d951d
DenVik <denyagarkusha2@gmail.com>
2025-02-01 13:05:26 +0100

    fix

--- src/utils/saveFile.js
+++ src/utils/saveFile.js
@@ -1,4 +1,4 @@
-import getEnvVar from './getEnvVar.js';
+import { getEnvVar } from './getEnvVar.js';
 import { saveFileToCloudinary } from './saveFileToCloudinary.js';
 import { saveFileToUploadDir } from './saveFileToUploadDir.js';
 
